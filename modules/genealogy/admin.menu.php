6b70e1f411e0274b7f31e2a0d26a0e1e18e81f9b
Nguyễn Thanh Hoàng <hoang.nguyen@webvang.vn>
2017-10-02 13:54:44 +0700

    create file lang for site

---
+++ modules/genealogy/admin.menu.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author NukeViet Systems (hoangnt@nguyenvan.vn)
+ * @License GNU/GPL version 2 or any later version
+ */
+ 
+
+ 
+ 
---
+++ modules/genealogy/language/vi.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author NukeViet Systems (hoangnt@nguyenvan.vn)
+ * @License GNU/GPL version 2 or any later version
+ */
+ 
+if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+ 
+$lang_translator['author'] ="NukeViet Systems (hoangnt@nguyenvan.vn)";
+$lang_translator['createdate'] ="02/10/2017, 15:22";
+$lang_translator['copyright'] ="@Copyright (C) 2012 nukeviet.systems. All rights reserved";
+$lang_translator['info'] ="";
+$lang_translator['langtype'] ="lang_module";
+
+$lang_module['family'] = "Họ";
+$lang_module['u_edit'] = "Sửa thành viên";
+$lang_module['u_weight'] = "Là con/vợ thứ mấy";
+$lang_module['u_lev'] = "Đời thứ";
+$lang_module['u_relationships'] = "Quan hệ với người được chọn";
+$lang_module['u_relationships_1'] = "Con";
+$lang_module['u_relationships_2'] = "Vợ";
+$lang_module['u_relationships_3'] = "Chồng";
+
+$lang_module['u_gender'] = "Giới tính";
+$lang_module['u_gender_0'] = "Chưa biết";
+$lang_module['u_gender_1'] = "Nam";
+$lang_module['u_gender_2'] = "Nữ";
+$lang_module['u_status'] = "Tình trạng";
+$lang_module['u_status_0'] = "Đã mất";
+$lang_module['u_status_1'] = "Còn sống";
+$lang_module['u_status_2'] = "Không rõ";
+$lang_module['u_full_name'] = "Tên húy";
+$lang_module['u_code'] = "Số mã hiệu trong gia phả";
+$lang_module['u_code_if_exit'] = "nếu có";
+$lang_module['u_name1'] = "Tên tự (Là tên tự gọi)";
+$lang_module['u_name2'] = "Tên thụy phong";
+$lang_module['u_birthday'] = "Ngày giờ sinh";
+$lang_module['u_dieday'] = "Ngày giờ mất";
+$lang_module['u_life_ht'] = "Hưởng thọ";
+$lang_module['u_life_hd'] = "Hưởng dương";
+$lang_module['u_burial'] = "Mộ táng tại";
+$lang_module['u_content'] = "Sự nghiệp, công đức";
+$lang_module['u_image'] = "Ảnh chân dung";
+$lang_module['u_address'] = "Địa chỉ hiện tại";
+$lang_module['u_mother'] = "Là con của";
+$lang_module['u_life'] = "Hưởng thọ/hưởng dương";
+
+$lang_module['list_parentid_0'] = "Danh sách anh em";
+$lang_module['list_parentid_1'] = "Danh sách con cái";
+$lang_module['list_parentid_2'] = "Danh sách vợ";
+
+$lang_module['name'] = "Tiêu đề";
+$lang_module['function'] = "Chức năng";
+$lang_module['status'] = "Hiển thị công cộng";
+$lang_module['anniversary'] = "Ngày giỗ";
+$lang_module['anniversary1'] = "Thêm ngày giỗ";
+$lang_module['save'] = "Lưu lại";
+$lang_module['addnews'] = "Thêm mới gia phả";
+
+$lang_module['who_view'] = "Thành viên được xem";
+$lang_module['who_view0'] = "Tất cả";
+$lang_module['who_view1'] = "Thành viên";
+$lang_module['who_view2'] = "Chỉ người tạo và quản trị viên";
+
+$lang_module['error_who_view_title'] = "Lỗi: phân quyền xem Gia phả";
+$lang_module['error_who_view_content'] = "Lỗi: bạn không có đủ quyền hạn để xem gia phả, nếu bạn chưa đăng nhập vui lòng đăng nhập hoặc đăng ký thành viên";
+
+$lang_module['error_login_title'] = "Thông báo đăng nhập";
+$lang_module['error_login_content'] = "Để sử dụng chức năng này hệ thống yêu cầu bạn đăng nhập thành viên,sau khi đăng nhập bạn có thể sử dụng chức năng này.";
+
+$lang_module['search'] = 'Tìm kiếm gia phả';
+$lang_module['search_enter'] = 'Nhập từ khóa...';
+$lang_module['search_result'] = 'Tìm thấy %s kết quả';
+$lang_module['search_location'] = 'Thuộc địa điểm';
+$lang_module['search_family'] = 'Dòng họ';
+$lang_module['search_family'] = 'Dòng họ';
+$lang_module['search_year'] = 'Năm biên soạn';
+$lang_module['search_author'] = 'Người biên soạn';
+
+$lang_module['search_persion'] = 'Tìm kiếm người';
+$lang_module['search_persionu_gender'] = "Giới tính";
+$lang_module['search_persionu_gender_0'] = "Giới tính";
+$lang_module['search_persionu_gender_1'] = "Chưa biết";
+$lang_module['search_persionu_gender_2'] = "Nam";
+$lang_module['search_persionu_gender_3'] = "Nữ";
+$lang_module['search_persionu_status'] = "Tình trạng";
+$lang_module['search_persionu_status_0'] = "Tình trạng";
+$lang_module['search_persionu_status_1'] = "Đã mất";
+$lang_module['search_persionu_status_2'] = "Còn sống";
+$lang_module['search_persionu_status_3'] = "Không rõ";
+$lang_module['search_ingp'] = "Gia phả";
+
+$lang_module['search_stt'] = "STT";
+$lang_module['search_full_name'] = "Tên khai sinh, CMND";
+$lang_module['search_nq'] = "Nguyên quán";
+
+$lang_module['search_gtitle'] = "Chi họ";
+$lang_module['search_provice'] = "Tỉnh";
+$lang_module['search_maxlev'] = "Tổng số đời";
+$lang_module['search_tongthanhvien'] = "Tổng thành viên";
+$lang_module['search_full_name'] = "Người liên hệ";
+$lang_module['search_telephone'] = "Điện thoại";
+$lang_module['search_email'] = "email";
