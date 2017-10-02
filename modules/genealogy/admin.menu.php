28fcea7c5624f7cae93aaf1b4922facece8676e8
Nguyễn Thanh Hoàng <hoang.nguyen@webvang.vn>
2017-10-02 14:14:10 +0700

    create file admin.menu.php

--- modules/genealogy/admin.menu.php
+++ modules/genealogy/admin.menu.php
@@ -5,7 +5,13 @@
  * @Author NukeViet Systems (hoangnt@nguyenvan.vn)
  * @License GNU/GPL version 2 or any later version
  */
- 
 
+if( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) or ! defined( 'NV_IS_MODADMIN' ) ) die( 'Stop!!!' );
+
+$submenu['genealogy'] = $lang_module['genealogy'];
+$submenu['location'] = $lang_module['location'];
+$submenu['family'] = $lang_module['family'];
  
+$allow_func = array('main', 'config', 'location', 'location_del', 'alias', 'change_status_location', 'change_weight_location', 'family', 'chang_family', 'list_family', 'del_family', 'genealogy', 'list_genealogy', 'chang_genealogy', 'del_genealogy', 'anniversary', 'genealogy_show', 'users');
+
  
